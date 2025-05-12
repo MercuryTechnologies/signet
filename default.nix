b5e62f98e912e5c093bf761f89d062433c24ea4c
Taylor Fausak <tfausak@mercury.com>
2025-05-12 16:48:04 -0500

    Pin nixpkgs

--- default.nix
+++ default.nix
@@ -1,12 +1,12 @@
 let
-  pkgs = import <nixpkgs> { };
+  pkgs = import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-24.11.tar.gz") {};
 in
-pkgs.haskell.packages.ghc98.developPackage {
+pkgs.haskellPackages.developPackage {
   root = ./.;
   modifier =
     drv:
     pkgs.haskell.lib.addBuildTools drv (
-      with pkgs.haskell.packages.ghc98;
+      with pkgs.haskellPackages;
       [
         cabal-install
         cabal-gild
